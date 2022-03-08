224e5916ca29728837d90765e360e496a16e3988
Hiago Garcia <garcia_hiago@hotmail.com>
2022-03-07 16:28:04 -0800

    commit

---
+++ Python/Fundeamentals/Fundamentals/test.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    x=a+b
+    return x
+new_val=add(3, 5)
+print(new_val)
+""" 
+def say_hi(name):
+    print("Hi, " + name)
+    # invoking the function 3 times, passing in one argument each time
+say_hi('Michael')
+say_hi('Anna')
+say_hi('Eli')
+
+"""
