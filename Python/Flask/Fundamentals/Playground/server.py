b9da453bb4e9868135a20bcad010dd2cf612c0b4
Hiago Garcia <garcia_hiago@hotmail.com>
2022-03-10 15:02:35 -0800

    more folder movement

--- Python/Flask/Fundamentals/Playground/server.py
+++ Python/Flask/Fundamentals/Playground/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/play')
+@app.route('/')
 def index():
-    return render_template('playground.html', box = '', times = 3)
+    return render_template('playground.html')
+
+@app.route('/play')
+def boxes():
+    
 
 
 if __name__=="__main__":
