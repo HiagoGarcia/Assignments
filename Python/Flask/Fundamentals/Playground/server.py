dc426653bce4e3d465352a0fa565e891fc1a2e67
Hiago Garcia <garcia_hiago@hotmail.com>
2022-03-10 16:37:33 -0800

    commit

--- Python/Flask/Fundamentals/Playground/server.py
+++ Python/Flask/Fundamentals/Playground/server.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('playground.html')
-
 @app.route('/play')
-def boxes():
-    
+def level1():
+    return render_template('playground.html', times = 3)
 
+@app.route('/play/<num>')
+def level2(num):
+    return render_template('playground.html', times = int(num))
+
+@app.route('/play/<num>/<color>')
+def level3(num,color):
+    return render_template('playground.html', times = int(num), b_c = str(color))
 
 if __name__=="__main__":
     app.run(debug=True)
