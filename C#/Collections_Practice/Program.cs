03eeeaee896d41a20e910d8e9d986905a6fdfea4
Hiago Garcia <garcia_hiago@hotmail.com>
2022-04-04 15:55:32 -0700

    added items

--- C#/Collections_Practice/Program.cs
+++ C#/Collections_Practice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections_Practice
 {
@@ -6,7 +7,41 @@ namespace Collections_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            Dictionary<string, string> likes = new Dictionary<string, string>();
+            //List of Flavors
+            List<string> ice_cream = new List<string>();
+            ice_cream.Add("Vanilla");
+            ice_cream.Add("Mint");
+            ice_cream.Add("Strawberry");
+            ice_cream.Add("CookiesNCreem");
+            ice_cream.Add("MooseTracks");
+            Console.WriteLine($"{ice_cream.Count}");
+            Console.WriteLine($"{ice_cream[2]}");
+            ice_cream.RemoveAt(2);
+            Console.WriteLine($"{ice_cream.Count}");
+            //Three Basic Arrays
+            int[] numArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            string[] Names = new string[] { "Tim", "Martin", "Nikki", "Sara" };
+            foreach (string name in Names)
+            {
+                int index = rnd.Next(ice_cream.Count);
+                string randomFlavor = ice_cream[index];
+                likes.Add(name, " " + randomFlavor);
+            }
+
+            bool[] EveryOther = new bool[10];
+            for (int i = 0; i < 10; i++)
+            {
+                bool Every = i % 2 == 0;
+                EveryOther[i] = Every;
+            }
+            //User Info Dictionary
+            foreach (KeyValuePair<string, string> entry in likes)
+            {
+                Console.WriteLine(entry.Key + entry.Value);
+            }
         }
     }
 }
