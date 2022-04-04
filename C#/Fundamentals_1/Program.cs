2e0c0046f85b6bb61ebef8c59041d2a3e8733727
Hiago Garcia <garcia_hiago@hotmail.com>
2022-04-04 13:38:36 -0700

    commit

---
+++ C#/Fundamentals_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Fundamentals_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // PrintDictionary();
+
+                for (int i = 0; i <= 255; i++){
+                    Console.WriteLine(i);
+                    }
+
+                for (int i = 1; i <= 100; i++){
+                    bool divisibleBy3 = i % 3 == 0;
+                    bool divisibleBy5 = i % 5 ==0;
+                    bool divisibleByBoth = divisibleBy3 && divisibleBy5;
+
+                    if (divisibleByBoth == false && (divisibleBy3 || divisibleBy5)){
+                        Console.WriteLine(i);
+                    }
+                }
+                for (int i =1; i <= 100; i++){
+                    bool divisibleBy3 = i % 3 == 0;
+                    bool divisibleBy5 = i % 5 ==0;
+                    bool divisibleByBoth = divisibleBy3 && divisibleBy5;
+
+                    if (divisibleByBoth){
+                        Console.WriteLine($"Fizz Buzz (i:{i})");
+                    }
+                    else if (divisibleBy3){
+                        Console.WriteLine($"Fizz (i:{i})");
+                    }
+                    else if (divisibleBy5){
+                        Console.WriteLine($"Buzz (i:{i})");
+                    }
+                    }
+                }
+    }
+}
