ffcbff9487458a301d1f34002fe8776a0a48cf55
Hiago Garcia <garcia_hiago@hotmail.com>
2022-04-04 20:26:25 -0700

    commit

---
+++ C#/Puzzles/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Puzzles
+{
+    class Program
+    {
+        static void RandomArray()
+        {
+            Random rnd = new Random();
+            for (int i = 5; i <= 25; i++)
+            {
+                int[] numbers = new int[] {rnd.Next()};
+            }
+        }
+        static void Main(string[] args)
+        {
+            RandomArray();
+        }
+    }
+}
