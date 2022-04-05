2839a065ffb1423e3d214d73ad5715d802a22abc
Hiago Garcia <garcia_hiago@hotmail.com>
2022-04-05 15:02:50 -0700

    commit

--- C#/Human/Program.cs
+++ C#/Human/Program.cs
@@ -9,4 +9,54 @@ namespace Human
             Console.WriteLine("Hello World!");
         }
     }
+
+    class Human
+    {
+        // Fields for Human
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Intelligence { get; set; }
+        public int Dexterity { get; set; }
+        private int health { get; set; }
+
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                if (health > 0)
+                {
+                    health = value;
+                }
+            }
+        }
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Dexterity = 3;
+            Intelligence = 3;
+            Health = 100;
+        }
+
+
+        public Human (string name, int strength, int intelligence, int dexterity, int health)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            Health = health;
+        }
+
+
+        public int Attack(Human target)
+        {
+            target.Health = target.Health - (target.Strength *5);
+            return target.Health;
+        }
+    }
 }
