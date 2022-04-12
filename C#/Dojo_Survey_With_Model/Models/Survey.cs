a09f5667fd63d3928df437f2c51f709f627edcce
Hiago Garcia <garcia_hiago@hotmail.com>
2022-04-12 12:52:02 -0700

    commit

---
+++ C#/Dojo_Survey_With_Model/Models/Survey.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dojo_Survey_With_Model.Models
+{
+    public class Dojo_Survay{
+        public string Name {get; set;}
+        public string Dojo {get; set;}
+        
+    }
+}
