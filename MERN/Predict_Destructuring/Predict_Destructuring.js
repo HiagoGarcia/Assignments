ec8a571f9089c8f83ba02618f2607fa7f0d9006c
Hiago Garcia <garcia_hiago@hotmail.com>
2022-05-02 14:01:58 -0700

    commit

---
+++ MERN/Predict_Destructuring/Predict_Destructuring.js
@@ -0,0 +1,14 @@
+//Problem 1
+//Outcome is 'Tesla, Mercedes'
+
+//Problem 2
+//Outcome is 'Erorr: "name" is not defined'
+
+//Problem 3
+//Outcome is '12345, undefined'
+
+//Problem 4
+//Outcome is 'false, true'
+
+//Problem 5
+//Outcome is 'value, [1, 5, 1, 8, 3, 3], 1, 5'
