5e00aa056399f68e2816946c208aa253821fbf6f
Hiago Garcia <garcia_hiago@hotmail.com>
2022-05-06 11:04:41 -0700

    commit

---
+++ MERN/First_JSX/first_jsx/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li><p>Learn React</p></li>
+        <li><p>Climb Mt. Everest</p></li>
+        <li><p>Run a marathon</p></li>
+        <li><p>Feed the dogs</p></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
